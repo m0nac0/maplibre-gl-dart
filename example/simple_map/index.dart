d94cfc94008307cd1d580e427407821086da1943
Julian Bissekkou <julian.bissekkou@tapped.dev>
2023-08-18 10:26:09 +0200

    revert changes to js depdenncy

--- example/simple_map/index.dart
+++ example/simple_map/index.dart
@@ -1,15 +1,25 @@
+import 'dart:html';
+
 import 'package:maplibre_gl_dart/mapbox_gl_dart.dart';
 
 void main() {
-  Mapbox.accessToken =
-      'pk.eyJ1IjoiYW5kcmVhNjg5IiwiYSI6ImNrNGlsYjhyZDBuYXoza213aWphOGNjZmoifQ.maw_5NsXejG1DoOeOi6hlQ';
-
-  MapboxMap(
+  final map = MapboxMap(
     MapOptions(
       container: 'map',
-      style: 'mapbox://styles/mapbox/dark-v10',
+      style:
+          'https://api.maptiler.com/maps/3dd4d51b-ae78-4074-8b31-b47a49f1b5ce/style.json?key=kZ5xAKKbPzxo3GeJ2odT',
       center: LngLat(7.68227, 45.06755),
       zoom: 12,
     ),
   );
+  Future<void>.delayed(const Duration(seconds: 2), () {
+    final style = map.getStyle();
+    window.console.info(style);
+    window.console.info(style.layers);
+
+    window.console.info('------------');
+    final style2 = map.style;
+    window.console.info(style2);
+    window.console.info(style2.layers);
+  });
 }
